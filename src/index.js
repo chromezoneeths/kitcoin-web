10d82d2c84333bbd61c52eacffda4e5b95f16795
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-16 13:19:14 -0500

    Increased some times

--- src/index.js
+++ src/index.js
@@ -10,17 +10,18 @@ let path = localStorage.getItem("overrideURL") || `${window.location.protocol ==
 		window.logged_in = true;
 		lkc.secret().then(sec => localStorage.secret = sec);
 	}, true);
-	setTimeout(updateCounts, 500);
+
+	// Update the number
+	setTimeout(updateCounts, 1000);
 	async function updateCounts () {
-		if (window.lkc) {
-			if (window.logged_in) {
-				console.log("Trying to update kitcoin count...");
-				$("coin-count").prop('count', await lkc.balance());
-				setTimeout(updateCounts, 5000);
-			} else {
-				console.log("Couldn't update since we're not logged in yet");
-				setTimeout(updateCounts, 10);
-			}
+		if (window.logged_in) {
+			console.log("Trying to update kitcoin count...");
+			let balance = await lkc.balance();
+			$("coin-count").prop('count', balance);
+			setTimeout(updateCounts, 60000);
+		} else {
+			console.log("Couldn't update since we're not logged in yet");
+			setTimeout(updateCounts, 10);
 		}
 	}
 })();
