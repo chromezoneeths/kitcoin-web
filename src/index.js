b3a30d3e07379b5049bd00528b4223732d218e0d
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-21 14:30:33 -0500

    Fixed url to pass same-origin on nonstandard ports

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let secret = {
 	get () { return localStorage.getItem('secret'); },
 	set (tgt) { localStorage.setItem('secret', tgt); }
 };
-let path = localStorage.getItem("overrideURL") || `${window.location.protocol === "https:" ? 'wss://' : 'ws://'}${window.location.hostname}/oauthinfo`; // oauthinfo is the only path guaranteed to be forwarded to kitcoin
+let path = localStorage.getItem("overrideURL") || `${window.location.protocol === "https:" ? 'wss://' : 'ws://'}${window.location.host}/oauth`; // oauth is the only path guaranteed to be forwarded to kitcoin.
 (async () => {
 	lkc.prepare(path, secret, () => {
 		window.logged_in = true;
