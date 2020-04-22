38c1662e89654a163051299ff5d50b4d513f73b7
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-22 18:45:25 -0500

    The logo now rotates when things are happening

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import lkc from 'libkitcoin';
+const lkc = require('libkitcoin');
 window.lkc = lkc;
-let loading = false;
+let loading = 0;
 const secret = {
-	get() {
+	get () {
 		return localStorage.getItem('secret');
 	},
-	set(tgt) {
+	set (tgt) {
 		localStorage.setItem('secret', tgt);
 	}
 };
 const path = localStorage.getItem('overrideURL') || `${window.location.protocol === 'https:' ? 'wss://' : 'ws://'}${window.location.host}/oauth`; // Oauth is the only path guaranteed to be forwarded to kitcoin.
 (async () => {
+	startLoading()
 	lkc.prepare(path, secret, () => {
 		window.logged_in = true;
+		stopLoading()
 		lkc.secret().then(sec => localStorage.secret = sec);
 	}, true);
 
 	// Update the number
 	setTimeout(updateCounts, 1000);
-	async function updateCounts() {
+	async function updateCounts () {
 		if (window.logged_in) {
 			console.log('Trying to update kitcoin count...');
 			const balance = await lkc.balance();
@@ -29,19 +31,44 @@ const path = localStorage.getItem('overrideURL') || `${window.location.protocol 
 			setTimeout(updateCounts, 10);
 		}
 	}
-
-	toggleLoading(true);
-
-	$('#toggle-ring-button').on('click', () => toggleLoading());
 })();
 
-function toggleLoading(target) {
-	loading = target || !loading;
-	console.log(`${loading ? 'Starting' : 'Stopping'} loading`);
-
-	if (loading) {
-		$('#loading-ring').removeClass('returning').addClass('rotating');
+function startLoading () {
+	loading++;
+	console.log(`Now waiting for ${loading} tasks...`);
+	if (loading === 1) {
+		$("#loading-ring").addClass('rotating');
+		$("#loading-ring").css("animation-duration", `${5 / loading}s`);
 	} else {
-		$('#loading-ring').removeClass('rotating').addClass('returning');
+		$("#loading-ring").unbind('animationiteration');
+		$("#loading-ring").one('animationiteration', (e) => {
+			$(e.target).removeClass('rotating');
+			var elm = e.target;
+			var elm2 = elm.cloneNode(true);
+			elm.parentNode.replaceChild(elm2, elm);
+			$(elm2).css("animation-duration", `${5 / loading}s`);
+			$(elm2).addClass('rotating')
+		});
+	}
+}
+
+function stopLoading () {
+	loading--;
+	console.log(`Now waiting for ${loading} tasks...`);
+	if (loading <= 0) {
+		$("#loading-ring").unbind('animationiteration');
+		$("#loading-ring").one('animationiteration', () => {
+			$("#loading-ring").removeClass('rotating');
+		});
+	} else {
+		$("#loading-ring").unbind('animationiteration');
+		$("#loading-ring").one('animationiteration', (e) => {
+			$(e.target).removeClass('rotating');
+			var elm = e.target;
+			var elm2 = elm.cloneNode(true);
+			elm.parentNode.replaceChild(elm2, elm);
+			$(elm2).css("animation-duration", `${5 / loading}s`);
+			$(elm2).addClass('rotating')
+		});
 	}
 }
